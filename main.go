244f1aebe56de71cb587410de6610cf4c078615e
José <walterandrade0001@gmail.com>
2025-07-17 18:17:57 -0300

    Adição de carregamento da API HTTP

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/jose-techcode/CLI_Luasys/cmd"
+	"github.com/jose-techcode/CLI_Luasys/api"
+	"os"
 )
 
 func main() {
-	cmd.Execute()
+	if len(os.Args) > 1 && os.Args[1] == "api" {
+		api.Server()
+	} else {
+		cmd.Execute()
+	}
 }
