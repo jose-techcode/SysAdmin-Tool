367cea5bf6f052dde06126d13a1a86ffeab96f5d
José <walterandrade0001@gmail.com>
2025-07-17 22:57:03 -0300

    Retiração de partes desnecessárias

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import (
+import(	
 	"github.com/jose-techcode/CLI_Luasys/cmd"
-	"github.com/jose-techcode/CLI_Luasys/api"
-	"os"
+
 )
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "api" {
-		api.Server()
-	} else {
-		cmd.Execute()
-	}
+	cmd.Execute()
 }
